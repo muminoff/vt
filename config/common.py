e78167a04a20dac41d88fe1d3f2e6874a11a2516
Sardor <sardor@hanbiro.com>
2015-05-14 22:24:04 +0900

    Separate apps

--- config/common.py
+++ config/common.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+BASE_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
+
+LOCAL_APPS = (
+    'core',
+)
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
