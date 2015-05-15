2caca5c1cbe1e41e15083cab59585a08d07d17a4
muminoff <smuminov@gmail.com>
2015-05-15 15:37:01 +0900

    Unit test boilerplate

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from core.models import User
 
-# Create your tests here.
+
+class UserCollectionTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_user(self):
+        pass
+
+    def test_read_user(self):
+        pass
+
+    def test_update_user(self):
+        pass
+
+    def test_delete_user(self):
+        pass
+
+    def test_delete_user(self):
+        pass
