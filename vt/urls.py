e3d494e4ade4816eb355d5e4bf2eaf0a066d89bb
muminoff <smuminov@gmail.com>
2015-05-15 13:29:46 +0900

    No serve static for local

--- vt/urls.py
+++ vt/urls.py
@@ -6,8 +6,3 @@ urlpatterns = [
     url(r'^users/$', 'core.views.user_collection'),
     url(r'^users/(?P<pk>[0-9]+)$', 'core.views.user_element')
 ]
-
-if settings.DEBUG:
-    urlpatterns = urlpatterns + [
-            url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
-]
