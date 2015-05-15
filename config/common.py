6afa7569c2b211083824756a0a1009c322e8de33
muminoff <smuminov@gmail.com>
2015-05-15 11:29:56 +0900

    Static files updated and pipeline made

--- config/common.py
+++ config/common.py
@@ -24,6 +24,7 @@ LOCAL_APPS = (
 
 THIRD_PARTY_APPS = (
     'rest_framework',
+    'pipeline',
 )
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_PARTY_APPS
@@ -87,6 +88,13 @@ STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
         )
 
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'pipeline.finders.PipelineFinder',
+        'pipeline.finders.CachedFileFinder',
+        )
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -107,3 +115,39 @@ TEMPLATES = [
         },
     },
 ]
+
+PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
+PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+PIPELINE_VERSIONING = 'pipeline.versioning.hash.MD5Versioning'
+PIPELINE_ENABLED = True
+PIPELINE_AUTO = True
+PIPELINE_VERSION = True
+
+PIPELINE_CSS = {
+        'bootstrap': {
+            'source_filenames': (
+                'css/bootstrap.css',
+                ),
+            'output_filename': 'css/bootstrap.min.css',
+            },
+        'bootstrap-tweaks': {
+            'source_filenames': (
+                'css/bootstrap-tweaks.css',
+                ),
+            'output_filename': 'css/bootstrap-tweaks.min.css',
+            },
+        'prettify': {
+            'source_filenames': (
+                'css/prettify.css',
+                ),
+            'output_filename': 'css/prettify.min.css',
+            },
+        'default': {
+            'source_filenames': (
+                'css/default.css',
+                ),
+            'output_filename': 'css/default.min.css',
+            },
+    }
+
--- vt/urls.py
+++ vt/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', 'core.views.api_doc'),
     url(r'^users/$', 'core.views.user_collection'),
     url(r'^users/(?P<pk>[0-9]+)$', 'core.views.user_element')
+]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + [
+            url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
 ]
