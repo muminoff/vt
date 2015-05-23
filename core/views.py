8bf84f1d148ab98c91a5e58bc43513bb8d4138a3
Sardor Muminov <smuminov@gmail.com>
2015-05-24 01:55:56 +0900

    Render template

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from core.models import User, Section
 from core.serializers import UserSerializer
 
 def api_doc(request):
-    return render(request, 'restframework/api_doc.html')
+    return render(request, 'rest_framework/api_doc.html')
 
 
 @api_view(['GET', 'POST'])
